f83137251562259f6e221525cd64c1afd959f70e
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2024-04-22 15:31:24 +0200

    bump eslint version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2024: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
